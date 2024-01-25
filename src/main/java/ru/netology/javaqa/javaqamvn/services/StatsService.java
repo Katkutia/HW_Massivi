60321806f52e0fe98483cd893cb6e80c876c3bb7
Katerina Platonova <platonovihome@gmail.com>
2024-01-25 14:56:37 +0300

    first commit

---
+++ src/main/java/ru/netology/javaqa/javaqamvn/services/StatsService.java
@@ -0,0 +1,60 @@
+package ru.netology.javaqa.javaqamvn.services;
+public class StatsService {
+    public long sumSales(long[] sales) {
+        long sum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            sum += sales[i];
+        }
+        return sum;
+    }
+
+    public long avgSales(long[] sales) {
+        long sum = sumSales(sales);
+        return sum / sales.length;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+
+        return minMonth + 1;
+    }
+
+    public int maxSales(long[] sales) {
+        int minMonth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+
+        return minMonth + 1;
+    }
+
+    public int lessThanAvgSales(long[] sales) {
+        long avg = avgSales(sales);
+        int count = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < avg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int moreThanAvgSales(long[] sales) {
+        long avg = avgSales(sales);
+        int count = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > avg) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
